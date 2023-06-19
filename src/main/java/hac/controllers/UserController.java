ad929ebbb7b4c5172a6a00298360d70a283ce28a
Nadav <ne600ne@gmail.com>
2023-06-19 15:31:55 +0300

    with user and admin

--- src/main/java/hac/controllers/UserController.java
+++ src/main/java/hac/controllers/UserController.java
@@ -4,6 +4,8 @@ import hac.repo.User;
 import hac.repo.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,20 +18,39 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class UserController {
 
+    private static final String ADMIN_USERNAME = "admin@gmail.com";
+    private static final String ADMIN_EMAIL = "admin@gmail.com";
+    private static final String ADMIN_PASSWORD = "admin@gmail.com";
+
     @Autowired
     private UserRepository repository;
 
+    @EventListener
+    public void registerAdmin(ApplicationEvent event) {
+        if (repository.findByUserName(ADMIN_USERNAME) == null) {
+            repository.save(new User(ADMIN_USERNAME, ADMIN_USERNAME, "1234"));
+        }
+    }
+
     @GetMapping("/")
-    public String login(Model model) {
+    public String login() {return "welcome";}
+
+    @GetMapping("/adminLogin")
+    public String adminLogin(Model model) {
         model.addAttribute("user", new User());
-        return "login";
+        return "admin/login";
+    }
+
+    @GetMapping("/userLogin")
+    public String userLogin(Model model) {
+        model.addAttribute("user", new User());
+        return "user/login";
     }
 
     @GetMapping("/register")
     public String register(Model model) {
-        User user = new User();
-        model.addAttribute("user", user);
-        return "register";
+        model.addAttribute("user", new User());
+        return "user/register";
     }
 
     @PostMapping("/addUser")
@@ -38,22 +59,20 @@ public class UserController {
        // validate the object and get the errors
         if (result.hasErrors()) {
             // errors will be displayed in the view
-            System.out.println("validation errors: " + result.getAllErrors());
             model.addAttribute("user", user);
-            return "register";
+            return "user/register";
         }
 
         try {
             repository.save(user);
-        } catch (DataIntegrityViolationException e) {
-            // Handle constraint violation error (duplicate userName)
+        } catch (DataIntegrityViolationException e) { // Handle duplicate userName
             result.rejectValue("userName", "error.userName", "Username already exists");
             model.addAttribute("user", user);
-            return "register";
+            return "user/register";
         }
 
        repository.save(user);
-        return "login";
+        return "user/login";
     }
 
     @PostMapping("/loginUser")
@@ -61,39 +80,55 @@ public class UserController {
 
         if (result.hasErrors()) {
             model.addAttribute("loginError", result.getAllErrors());
-            return "login";
+            return "user/login";
         }
-
+        // extract user from table
         User existingUser = repository.findByUserName(user.getUserName());
-        System.out.println(existingUser);
 
         if (existingUser == null) {
             result.rejectValue("userName", "error.userName", "user not found");
             model.addAttribute("user", user);
-            System.out.println("in not-found");
-            return "login";
+            return "user/login";
         }
         if (!user.getPassword().equals(existingUser.getPassword())) {
             result.rejectValue("password", "error.password", "wrong password");
             model.addAttribute("user", user);
-            System.out.println("in wrong password");
-            return "login";
+            return "user/login";
         }
 
-        // Successful login
 
+        // Successful login
         List<User> users = repository.findAll();
-        System.out.println(users);
         model.addAttribute("users", users);
-        System.out.println("in secssess");
-        return "users";
+        return "user/profiles";
     }
 
-    @GetMapping("/users")
-    public String showProfile(Model model) {
+    @PostMapping("/loginAdmin")
+    public String loginAdmin(@ModelAttribute("user") User user, BindingResult result, Model model) {
+
+        if (!(user.getPassword().equals(ADMIN_PASSWORD) && user.getUserName().equals(ADMIN_USERNAME))) {
+            result.rejectValue("password", "error.password", "wrong password or userName");
+            model.addAttribute("user", user);
+            return "admin/login";
+        }
+
         List<User> users = repository.findAll();
-        System.out.println(users);
         model.addAttribute("users", users);
-        return "users";
+        return "admin/profiles";
+    }
+
+
+        @GetMapping("/admin/profiles")
+    public String editProfiles(Model model) {
+        List<User> users = repository.findAll();
+        model.addAttribute("users", users);
+        return "admin/profiles";
+    }
+
+    @GetMapping("/user/profiles")
+    public String showProfiles(Model model) {
+        List<User> users = repository.findAll();
+        model.addAttribute("users", users);
+        return "user/profiles";
     }
 }
--- src/main/java/hac/repo/UserRepository.java
+++ src/main/java/hac/repo/UserRepository.java
@@ -1,22 +1,15 @@
 package hac.repo;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
 import java.util.List;
 
-/* default scope of this Bean is "singleton" */
 public interface UserRepository extends JpaRepository<User, Long> {
-//
-    /** SOME EXAMPLES:
-     *  defining some queries using the method names
-     *  Spring will implement the method for us based on the method name
-     *  https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods
-     */
+
     //User findByUserName(String userName);
     User findByUserName(String userName);
     User findByEmail(String email);
     @Query("SELECT u FROM User u ORDER BY u.userName ASC")
     List<User> findAllByUserName(String userName);
     List<User> findFirst10ByOrderByUserNameDesc(); // find first 10 users ordered by userName desc
-
 }
